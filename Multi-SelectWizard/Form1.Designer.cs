40b5e1d1f7863d0c390677c63064ae3a28c03ff4
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-03-21 19:06:11 +0200

    Add files via upload

---
+++ Multi-SelectWizard/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace Multi_SelectWizard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.leftListBox = new System.Windows.Forms.ListBox();
+            this.rightListBox = new System.Windows.Forms.ListBox();
+            this.allLeftButton = new System.Windows.Forms.Button();
+            this.allRightButton = new System.Windows.Forms.Button();
+            this.moveRightButton = new System.Windows.Forms.Button();
+            this.moveLeftButton = new System.Windows.Forms.Button();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // leftListBox
+            // 
+            this.leftListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.leftListBox.FormattingEnabled = true;
+            this.leftListBox.ItemHeight = 31;
+            this.leftListBox.Items.AddRange(new object[] {
+            "item1",
+            "item2",
+            "item3",
+            "item4",
+            "item5",
+            "item6",
+            "item7",
+            "item8",
+            "item9",
+            "item10"});
+            this.leftListBox.Location = new System.Drawing.Point(87, 88);
+            this.leftListBox.Name = "leftListBox";
+            this.leftListBox.Size = new System.Drawing.Size(373, 345);
+            this.leftListBox.TabIndex = 0;
+            // 
+            // rightListBox
+            // 
+            this.rightListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rightListBox.FormattingEnabled = true;
+            this.rightListBox.ItemHeight = 31;
+            this.rightListBox.Location = new System.Drawing.Point(732, 88);
+            this.rightListBox.Name = "rightListBox";
+            this.rightListBox.Size = new System.Drawing.Size(373, 345);
+            this.rightListBox.TabIndex = 1;
+            // 
+            // allLeftButton
+            // 
+            this.allLeftButton.Location = new System.Drawing.Point(490, 88);
+            this.allLeftButton.Name = "allLeftButton";
+            this.allLeftButton.Size = new System.Drawing.Size(75, 23);
+            this.allLeftButton.TabIndex = 2;
+            this.allLeftButton.Text = "All";
+            this.allLeftButton.UseVisualStyleBackColor = true;
+            // 
+            // allRightButton
+            // 
+            this.allRightButton.Location = new System.Drawing.Point(632, 410);
+            this.allRightButton.Name = "allRightButton";
+            this.allRightButton.Size = new System.Drawing.Size(75, 23);
+            this.allRightButton.TabIndex = 3;
+            this.allRightButton.Text = "All";
+            this.allRightButton.UseVisualStyleBackColor = true;
+            // 
+            // moveRightButton
+            // 
+            this.moveRightButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moveRightButton.Location = new System.Drawing.Point(563, 226);
+            this.moveRightButton.Name = "moveRightButton";
+            this.moveRightButton.Size = new System.Drawing.Size(75, 45);
+            this.moveRightButton.TabIndex = 4;
+            this.moveRightButton.Text = ">>>";
+            this.moveRightButton.UseVisualStyleBackColor = true;
+            this.moveRightButton.Click += new System.EventHandler(this.moveRightButton_Click);
+            // 
+            // moveLeftButton
+            // 
+            this.moveLeftButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moveLeftButton.Location = new System.Drawing.Point(563, 277);
+            this.moveLeftButton.Name = "moveLeftButton";
+            this.moveLeftButton.Size = new System.Drawing.Size(75, 45);
+            this.moveLeftButton.TabIndex = 5;
+            this.moveLeftButton.Text = "<<<";
+            this.moveLeftButton.UseVisualStyleBackColor = true;
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitBtn.Location = new System.Drawing.Point(333, 439);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(127, 45);
+            this.submitBtn.TabIndex = 6;
+            this.submitBtn.Text = "Submit";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetBtn.Location = new System.Drawing.Point(732, 439);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(107, 45);
+            this.resetBtn.TabIndex = 7;
+            this.resetBtn.Text = "Reset";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 553);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.moveLeftButton);
+            this.Controls.Add(this.moveRightButton);
+            this.Controls.Add(this.allRightButton);
+            this.Controls.Add(this.allLeftButton);
+            this.Controls.Add(this.rightListBox);
+            this.Controls.Add(this.leftListBox);
+            this.Name = "Form1";
+            this.Text = "Multi-Select Wizard";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox leftListBox;
+        private System.Windows.Forms.ListBox rightListBox;
+        private System.Windows.Forms.Button allLeftButton;
+        private System.Windows.Forms.Button allRightButton;
+        private System.Windows.Forms.Button moveRightButton;
+        private System.Windows.Forms.Button moveLeftButton;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.Button resetBtn;
+    }
+}
+
---
+++ Multi-SelectWizard/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multi_SelectWizard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            leftListBox.Items.Clear();
+            rightListBox.Items.Clear();
+            leftListBox.SelectionMode = SelectionMode.MultiExtended;
+            rightListBox.SelectionMode = SelectionMode.MultiExtended;
+
+            for(int i = 0; i < 9; i++)
+            {
+                leftListBox.Items.Insert(i, "Item " + i);
+            }
+
+            leftListBox.Sorted = true;
+            rightListBox.Sorted = true;
+        }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            leftListBox.Items.Clear();
+            rightListBox.Items.Clear();
+            leftListBox.SelectionMode = SelectionMode.MultiExtended;
+            rightListBox.SelectionMode = SelectionMode.MultiExtended;
+
+            for (int i = 0; i < 9; i++)
+            {
+                leftListBox.Items.Insert(i, "Item " + i);
+            }
+
+            leftListBox.Sorted = true;
+            rightListBox.Sorted = true;
+        }
+
+        private void moveRightButton_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i <= leftListBox.Items.Count - 1; i++)
+            {
+                if(leftListBox.GetSelected(i) == true)
+                {
+                    rightListBox.Items.Add(leftListBox.Items[i]);
+                }
+            }
+
+            //now invert the scan for the removes
+            for(int i = leftListBox.Items.Count - 1; i >= 0; i--)
+            {
+                if(leftListBox.GetSelected(i) == true)
+                {
+                    leftListBox.Items.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
